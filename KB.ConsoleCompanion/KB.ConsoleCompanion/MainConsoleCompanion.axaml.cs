eb6d53c22def635043b976a708e39dbc5b620c9c
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-11-02 16:50:59 +0100

    add test response of the server

--- KB.ConsoleCompanion/KB.ConsoleCompanion/MainConsoleCompanion.axaml.cs
+++ KB.ConsoleCompanion/KB.ConsoleCompanion/MainConsoleCompanion.axaml.cs
@@ -2,6 +2,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using ConsoleCompanionAPI;
+using ConsoleCompanionAPI.Data;
 using ConsoleCompanionAPI.Interfaces;
 
 namespace KB.ConsoleCompanion;
@@ -14,6 +15,13 @@ public partial class MainConsoleCompanion : UserControl
         //Test
         _server = ProtocolFactory.CreateServer();
         _server.Start("127.0.0.1", "55555");
+
+        _server.OnCommandReceived += (command) =>
+        {
+            ConsoleCommand response = new ConsoleCommand($"Response from command '{command.Command}' received sucessfully!", ConsoleCommand.ECommandType.Info);
+            return response;
+        };
+
         // End Test
         InitializeComponent();
     }
