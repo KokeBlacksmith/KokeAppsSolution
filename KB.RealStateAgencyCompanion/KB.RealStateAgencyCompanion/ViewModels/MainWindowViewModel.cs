ac2d9a9e1782345b9004b3f4e97f3e189ac626ff
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-11-03 22:18:22 +0100

    removed projects

--- KB.RealStateAgencyCompanion/KB.RealStateAgencyCompanion/ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KB.RealStateAgencyCompanion.ViewModels
-{
-    public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
-}
