f40b7e52ff55967f5b24d423e302c157946e983c
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-11-18 10:42:20 +0100

    remove vs files

---
+++ KB.AvaloniaCore/Controls/GraphEditor/Node.Properties.cs
@@ -0,0 +1,106 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Metadata;
+using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB.AvaloniaCore.Controls.GraphEditor;
+
+/// <summary>
+/// Styled properties of a node in a graph.
+/// </summary>
+public abstract partial class Node
+{
+    static Node()
+    {
+        Node.ChildProperty.Changed.AddClassHandler<Node>((s, e) => s.m_OnChildPropertyChanged(e));
+        Node.BackgroundProperty.Changed.AddClassHandler<Node>((s, e) => s._OnBackgroundPropertyChanged(e));
+        Node.BorderBrushProperty.Changed.AddClassHandler<Node>((s, e) => s._OnBorderBrushPropertyChanged(e));
+        Node.BorderThicknessProperty.Changed.AddClassHandler<Node>((s, e) => s._OnBorderThicknessPropertyChanged(e));
+        Node.CornerRadiusProperty.Changed.AddClassHandler<Node>((s, e) => s._OnCornerRadiusPropertyChanged(e));
+        Node.PaddingProperty.Changed.AddClassHandler<Node>((s, e) => s._OnPaddingPropertyChanged(e));
+    }
+
+    #region StyledProperties
+
+    public static readonly StyledProperty<Control?> ChildProperty = AvaloniaProperty.Register<Node, Control?>(nameof(Node.Child));
+    public static readonly StyledProperty<double> PositionXProperty = AvaloniaProperty.Register<Node, double>(nameof(Node.PositionX));
+    public static readonly StyledProperty<double> PositionYProperty = AvaloniaProperty.Register<Node, double>(nameof(Node.PositionY));
+
+    public static readonly StyledProperty<bool> IsSelectedProperty = AvaloniaProperty.Register<Node, bool>(nameof(Node.IsSelected));
+    public static readonly StyledProperty<bool> IsDraggedProperty = AvaloniaProperty.Register<Node, bool>(nameof(Node.IsDragged));
+
+    public static readonly StyledProperty<IBrush> BackgroundProperty = AvaloniaProperty.Register<Node, IBrush>(nameof(Node.Background), Brushes.White);
+    public static readonly StyledProperty<IBrush> BorderBrushProperty = AvaloniaProperty.Register<Node, IBrush>(nameof(Node.BorderBrush), Brushes.Black);
+    public static readonly StyledProperty<Thickness> BorderThicknessProperty = AvaloniaProperty.Register<Node, Thickness>(nameof(Node.BorderThickness), new Thickness(1));
+    public static readonly StyledProperty<CornerRadius> CornerRadiusProperty = AvaloniaProperty.Register<Node, CornerRadius>(nameof(Node.CornerRadius), new CornerRadius(0));
+    public static readonly StyledProperty<Thickness> PaddingProperty = AvaloniaProperty.Register<Node, Thickness>(nameof(Node.Padding), new Thickness(0));
+
+
+    [Content]
+    public Control? Child
+    {
+        get { return GetValue(Node.ChildProperty); }
+        set { SetValue(Node.ChildProperty, value); }
+    }
+
+    public double PositionX
+    {
+        get { return GetValue(Node.PositionXProperty); }
+        set { SetValue(Node.PositionXProperty, value); }
+    }
+
+    public double PositionY
+    {
+        get { return GetValue(Node.PositionYProperty); }
+        set { SetValue(Node.PositionYProperty, value); }
+    }
+
+    public bool IsSelected
+    {
+        get { return GetValue(Node.IsSelectedProperty); }
+        set { SetValue(Node.IsSelectedProperty, value); }
+    }
+
+    public bool IsDragged
+    {
+        get { return GetValue(Node.IsDraggedProperty); }
+        set { SetValue(Node.IsDraggedProperty, value); }
+    }
+
+    public IBrush Background
+    {
+        get { return GetValue(Node.BackgroundProperty); }
+        set { SetValue(Node.BackgroundProperty, value); }
+    }
+
+    public IBrush BorderBrush
+    {
+        get { return GetValue(Node.BorderBrushProperty); }
+        set { SetValue(Node.BorderBrushProperty, value); }
+    }
+
+    public Thickness BorderThickness
+    {
+        get { return GetValue(Node.BorderThicknessProperty); }
+        set { SetValue(Node.BorderThicknessProperty, value); }
+    }
+
+    public CornerRadius CornerRadius
+    {
+        get { return GetValue(Node.CornerRadiusProperty); }
+        set { SetValue(Node.CornerRadiusProperty, value); }
+    }
+
+    public Thickness Padding
+    {
+        get { return GetValue(Node.PaddingProperty); }
+        set { SetValue(Node.PaddingProperty, value); }
+    }
+
+    #endregion
+}
--- KB.AvaloniaCore/Controls/GraphEditor/Node.cs
+++ KB.AvaloniaCore/Controls/GraphEditor/Node.cs
@@ -9,43 +9,35 @@ using Avalonia.Metadata;
 
 namespace KB.AvaloniaCore.Controls.GraphEditor;
 
-public abstract class Node : Control
+public abstract partial class Node : Control
 {
     /// <summary>
     /// Container of all the elements of the node.
     /// </summary>
-    private readonly Panel? _panel;
+    private readonly Panel? _mainPanel;
 
     /// <summary>
     /// Container to add node connections to.
     /// </summary>
     /// 
-    private readonly Canvas? _canvas;
+    private readonly Canvas? _connectionsCanvas;
 
     /// <summary>
     /// Container of the <see cref="Child"/> control
     /// </summary>
-    private readonly Border? _border;
+    private readonly Border? _childParentBorder;
 
-    static Node()
-    {
-        Node.ChildProperty.Changed.AddClassHandler<Node>((s, e) => s.m_OnChildPropertyChanged(e));
-        Node.BackgroundProperty.Changed.AddClassHandler<Node>((s, e) => s._OnBackgroundPropertyChanged(e));
-        Node.BorderBrushProperty.Changed.AddClassHandler<Node>((s, e) => s._OnBorderBrushPropertyChanged(e));
-        Node.BorderThicknessProperty.Changed.AddClassHandler<Node>((s, e) => s._OnBorderThicknessPropertyChanged(e));
-        Node.CornerRadiusProperty.Changed.AddClassHandler<Node>((s, e) => s._OnCornerRadiusPropertyChanged(e));
-        Node.PaddingProperty.Changed.AddClassHandler<Node>((s, e) => s._OnPaddingPropertyChanged(e));
-    }
+
 
     public Node()
     {
-        _canvas = new Canvas()
+        _connectionsCanvas = new Canvas()
         {
             HorizontalAlignment = HorizontalAlignment.Stretch,
             VerticalAlignment = VerticalAlignment.Stretch,
         };
 
-        _border = new Border()
+        _childParentBorder = new Border()
         {
             HorizontalAlignment = HorizontalAlignment.Stretch,
             VerticalAlignment = VerticalAlignment.Stretch,
@@ -54,133 +46,59 @@ public abstract class Node : Control
             BorderThickness = new Thickness(1),
         };
 
-        _panel = new Panel()
+        _mainPanel = new Panel()
         {
             HorizontalAlignment = HorizontalAlignment.Stretch,
             VerticalAlignment = VerticalAlignment.Stretch,
         };
 
-        _panel.Children.Add(_canvas);
-        _panel.Children.Add(_border);
+        _mainPanel.Children.Add(_connectionsCanvas);
+        _mainPanel.Children.Add(_childParentBorder);
 
-        ((ISetLogicalParent)_panel).SetParent(this);
-        VisualChildren.Add(_panel);
-        LogicalChildren.Add(_panel);
+        ((ISetLogicalParent)_mainPanel).SetParent(this);
+        VisualChildren.Add(_mainPanel);
+        LogicalChildren.Add(_mainPanel);
     }
 
-    #region StyledProperties
-
-    public static readonly StyledProperty<Control?> ChildProperty = AvaloniaProperty.Register<Node, Control?>(nameof(Node.Child));
-    public static readonly StyledProperty<double> PositionXProperty = AvaloniaProperty.Register<Node, double>(nameof(Node.PositionX));
-    public static readonly StyledProperty<double> PositionYProperty = AvaloniaProperty.Register<Node, double>(nameof(Node.PositionY));
-    
-    public static readonly StyledProperty<bool> IsSelectedProperty = AvaloniaProperty.Register<Node, bool>(nameof(Node.IsSelected));
-    public static readonly StyledProperty<bool> IsDraggedProperty = AvaloniaProperty.Register<Node, bool>(nameof(Node.IsDragged));
-
-    public static readonly StyledProperty<IBrush> BackgroundProperty = AvaloniaProperty.Register<Node, IBrush>(nameof(Node.Background), Brushes.White);
-    public static readonly StyledProperty<IBrush> BorderBrushProperty = AvaloniaProperty.Register<Node, IBrush>(nameof(Node.BorderBrush), Brushes.Black);
-    public static readonly StyledProperty<Thickness> BorderThicknessProperty = AvaloniaProperty.Register<Node, Thickness>(nameof(Node.BorderThickness), new Thickness(1));
-    public static readonly StyledProperty<CornerRadius> CornerRadiusProperty = AvaloniaProperty.Register<Node, CornerRadius>(nameof(Node.CornerRadius), new CornerRadius(0));
-    public static readonly StyledProperty<Thickness> PaddingProperty = AvaloniaProperty.Register<Node, Thickness>(nameof(Node.Padding), new Thickness(0));
-
-
-    [Content]
-    public Control? Child
-    {
-        get { return GetValue(Node.ChildProperty); }
-        set { SetValue(Node.ChildProperty, value); }
-    }
-
-    public double PositionX
-    {
-        get { return GetValue(Node.PositionXProperty); }
-        set { SetValue(Node.PositionXProperty, value); }
-    }
-
-    public double PositionY
-    {
-        get { return GetValue(Node.PositionYProperty); }
-        set { SetValue(Node.PositionYProperty, value); }
-    }
-
-    public bool IsSelected
-    {
-        get { return GetValue(Node.IsSelectedProperty); }
-        set { SetValue(Node.IsSelectedProperty, value); }
-    }
-
-    public bool IsDragged
-    {
-        get { return GetValue(Node.IsDraggedProperty); }
-        set { SetValue(Node.IsDraggedProperty, value); }
-    }
-
-    public IBrush Background
-    {
-        get { return GetValue(Node.BackgroundProperty); }
-        set { SetValue(Node.BackgroundProperty, value); }
-    }
-
-    public IBrush BorderBrush
-    {
-        get { return GetValue(Node.BorderBrushProperty); }
-        set { SetValue(Node.BorderBrushProperty, value); }
-    }
-
-    public Thickness BorderThickness
-    {
-        get { return GetValue(Node.BorderThicknessProperty); }
-        set { SetValue(Node.BorderThicknessProperty, value); }
-    }
-
-    public CornerRadius CornerRadius
-    {
-        get { return GetValue(Node.CornerRadiusProperty); }
-        set { SetValue(Node.CornerRadiusProperty, value); }
-    }
-
-    public Thickness Padding
-    {
-        get { return GetValue(Node.PaddingProperty); }
-        set { SetValue(Node.PaddingProperty, value); }
-    }
-
-    #endregion
 
     #region Inhertied Members
 
-  
+
 
     #endregion
 
+    #region PropertyChangedEvents
+
     private void m_OnChildPropertyChanged(AvaloniaPropertyChangedEventArgs e)
     {
-        _border!.Child = (Control?)e.NewValue; ;
+        _childParentBorder!.Child = (Control?)e.NewValue; ;
     }
 
     private void _OnBackgroundPropertyChanged(AvaloniaPropertyChangedEventArgs e)
     {
-        _border!.Background = e.NewValue as IBrush;
+        _childParentBorder!.Background = e.NewValue as IBrush;
     }
 
     private void _OnBorderBrushPropertyChanged(AvaloniaPropertyChangedEventArgs e)
     {
-        _border!.BorderBrush = e.NewValue as IBrush;
+        _childParentBorder!.BorderBrush = e.NewValue as IBrush;
     }
 
     private void _OnBorderThicknessPropertyChanged(AvaloniaPropertyChangedEventArgs e)
     {
-        _border!.BorderThickness = (Thickness)e.NewValue!;
+        _childParentBorder!.BorderThickness = (Thickness)e.NewValue!;
     }
 
 
     private void _OnCornerRadiusPropertyChanged(AvaloniaPropertyChangedEventArgs e)
     {
-        _border!.CornerRadius = (CornerRadius)e.NewValue!;
+        _childParentBorder!.CornerRadius = (CornerRadius)e.NewValue!;
     }
 
     private void _OnPaddingPropertyChanged(AvaloniaPropertyChangedEventArgs e)
     {
-        _border!.Padding = (Thickness)e.NewValue!;
+        _childParentBorder!.Padding = (Thickness)e.NewValue!;
     }
+
+    #endregion
 }
