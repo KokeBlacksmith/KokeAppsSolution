63656166963c4b3c53b414dd1a05c5d29ff27c2b
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-12-19 19:04:08 +0100

    bezier line correctly drawn

--- KB.SharpCore/Utils/Math.cs
+++ KB.SharpCore/Utils/Math.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,22 @@ namespace KB.SharpCore.Utils;
 
 public static class Math
 {
-    public static bool IsBetween<T>(this T item, T start, T end)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsBetween<T>(this T item, in T start, in T end)
         where T : IComparable, IComparable<T>
     {
         return Comparer<T>.Default.Compare(item, start) >= 0 && Comparer<T>.Default.Compare(item, end) <= 0;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double DegToRad(in double deg)
+    {
+        return deg * System.Math.PI / 180.0;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double RadToDeg(in double rad)
+    {
+        return rad * 180.0 / System.Math.PI;
     }
 }
