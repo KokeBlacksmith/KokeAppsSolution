0ea8b2485e61ffe4c1ba19c2814a542f43643f7d
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-12-24 13:20:11 +0100

    utils for drawing grid background

---
+++ KB.AvaloniaCore/Injection/RenderBackgroundUtils.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace KB.AvaloniaCore.Injection;
+
+public static class RenderBackgroundUtils
+{
+    public static void DrawGrid(DrawingContext context, Rect bounds, IBrush lineBrush, double spacing)
+    {
+        Pen pen = new Pen(lineBrush, 1);
+
+        for (double x = 0; x < bounds.Width; x += spacing)
+        {
+            context.DrawLine(pen, new Point(x, 0), new Point(x, bounds.Height));
+        }
+
+        for (double y = 0; y < bounds.Height; y += spacing)
+        {
+            context.DrawLine(pen, new Point(0, y), new Point(bounds.Width, y));
+        }
+    }
+}
--- KB.ConsoleCompanion/KB.ConsoleCompanion/MacroEditView/MacroEditView.axaml.cs
+++ KB.ConsoleCompanion/KB.ConsoleCompanion/MacroEditView/MacroEditView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using KB.AvaloniaCore.Injection;
 
 namespace KB.ConsoleCompanion.MacroEditView;
 
@@ -14,23 +15,6 @@ public partial class MacroEditView : UserControl
     public override void Render(DrawingContext context)
     {
         base.Render(context);
-
-        DrawGrid(context);
-    }
-
-    private void DrawGrid(DrawingContext context)
-    {
-        Pen pen = new Pen(Brushes.LightGray, 1);
-        double spacing = 25;
-
-        for (double x = 0; x < Bounds.Width; x += spacing)
-        {
-            context.DrawLine(pen, new Point(x, 0), new Point(x, Bounds.Height));
-        }
-
-        for (double y = 0; y < Bounds.Height; y += spacing)
-        {
-            context.DrawLine(pen, new Point(0, y), new Point(Bounds.Width, y));
-        }
+        RenderBackgroundUtils.DrawGrid(context, Bounds, Brushes.LightGray, 25);
     }
 }
