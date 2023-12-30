8b6c3306ccd61b79fb31c5b0368e150b85dc097c
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-12-30 11:31:46 +0100

    datetime to datetetim offset converter

---
+++ KB.AvaloniaCore/Converters/DateTimeToDateTimeOffsetConverter.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace KB.AvaloniaCore.Converters;
+
+public class DateTimeToDateTimeOffsetConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        DateTime dateTime = DateTime.MinValue;
+        if (value is DateTime dateTimeValue)
+        {
+            dateTime = dateTimeValue;
+        }
+        else if (value is DateTime?)
+        {
+            DateTime? nullableDateTime = value as DateTime?;
+            return nullableDateTime.HasValue ? dateTime = nullableDateTime.Value : DateTime.MinValue;
+        }
+
+        // DateTimeOffset does not accept DatTime.MinValue
+        if (dateTime != DateTime.MinValue)
+        {
+            return new DateTimeOffset(dateTime);
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.DateTime;
+        }
+        else if (value is DateTimeOffset?)
+        {
+            DateTimeOffset? nullableDateTimeOffset = value as DateTimeOffset?;
+            return nullableDateTimeOffset.HasValue ? nullableDateTimeOffset.Value.DateTime : value;
+        }
+
+        return value;
+    }
+}
