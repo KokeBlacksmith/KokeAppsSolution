d50eeaf5dcdde37d2d0455bdb9d97e0e99c335e8
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2023-12-30 13:52:22 +0100

    refactor datetime converter

--- KB.AvaloniaCore/Converters/DateTimeToDateTimeOffsetConverter.cs
+++ KB.AvaloniaCore/Converters/DateTimeToDateTimeOffsetConverter.cs
@@ -7,40 +7,56 @@ public class DateTimeToDateTimeOffsetConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        DateTime dateTime = DateTime.MinValue;
+        if (value == null)
+        {
+            return null;
+        }
+
+        DateTime result = DateTime.MinValue;
         if (value is DateTime dateTimeValue)
         {
-            dateTime = dateTimeValue;
+            result = dateTimeValue;
         }
         else if (value is DateTime?)
         {
             DateTime? nullableDateTime = value as DateTime?;
-            return nullableDateTime.HasValue ? dateTime = nullableDateTime.Value : DateTime.MinValue;
-        }
-
-        // DateTimeOffset does not accept DatTime.MinValue
-        if (dateTime != DateTime.MinValue)
-        {
-            return new DateTimeOffset(dateTime);
+            result = nullableDateTime.HasValue ? nullableDateTime.Value : DateTime.MinValue;
         }
         else
         {
-            return value;
+            throw new ArgumentException($"Value is not a {nameof(DateTime)} or a {nameof(DateTime)}?");
+        }
+
+        // DateTimeOffset does not accept DatTime.MinValue
+        if (result != DateTime.MinValue)
+        {
+            return new DateTimeOffset(result);
+        }
+        else
+        {
+            return null;
         }
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        if (value == null)
+        {
+            return null;
+        }
+
         if (value is DateTimeOffset dateTimeOffset)
         {
             return dateTimeOffset.DateTime;
         }
         else if (value is DateTimeOffset?)
         {
-            DateTimeOffset? nullableDateTimeOffset = value as DateTimeOffset?;
-            return nullableDateTimeOffset.HasValue ? nullableDateTimeOffset.Value.DateTime : value;
+            DateTimeOffset? nullableDateTimeOffset = (DateTimeOffset?)value;
+            return nullableDateTimeOffset.HasValue ? nullableDateTimeOffset.Value.DateTime : null;
         }
-
-        return value;
+        else
+        {
+            throw new ArgumentException($"Value is not a {nameof(DateTimeOffset)} or a {nameof(DateTimeOffset)}?");
+        }
     }
 }
