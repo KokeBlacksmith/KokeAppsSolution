901d0d4e46746b3389f21c6e61c6f5e63fd00fb7
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2024-02-24 19:36:25 +0100

    init branch

---
+++ KB.AvaloniaCore/Controls/PropertyGrid/PropertyGrid.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace KB.AvaloniaCore.Controls
+{
+    public partial class PropertyGrid : UserControl
+    {
+        public PropertyGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
