3721962ad765a9d5f6f1fcefea19baad196a5e39
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2024-03-11 19:20:23 +0100

    drag items

--- KB.ConsoleCompanion/KB.ConsoleCompanion/MacroEditView/View/MacroEditView.axaml.cs
+++ KB.ConsoleCompanion/KB.ConsoleCompanion/MacroEditView/View/MacroEditView.axaml.cs
@@ -1,15 +1,5 @@
-using Avalonia;
-using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Media;
-using KB.AvaloniaCore.Controls;
-using KB.AvaloniaCore.Injection;
-using KB.SharpCore.Utils;
-using System;
-using System.Collections;
-using System.Collections.Specialized;
-using System.Reactive;
 
 namespace KB.ConsoleCompanion.MacroEditView;
 
